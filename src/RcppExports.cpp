a16e126c1bff6641f203e2e38d85906815d14f65
Iva Budimir <iva.budimir.os@gmail.com>
2021-06-29 02:42:35 +0200

    Empty vignettes, package, website

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,49 +6,6 @@
 
 using namespace Rcpp;
 
-// rcpparma_hello_world
-arma::mat rcpparma_hello_world();
-RcppExport SEXP _meNet_rcpparma_hello_world() {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    rcpp_result_gen = Rcpp::wrap(rcpparma_hello_world());
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_outerproduct
-arma::mat rcpparma_outerproduct(const arma::colvec& x);
-RcppExport SEXP _meNet_rcpparma_outerproduct(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_outerproduct(x));
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_innerproduct
-double rcpparma_innerproduct(const arma::colvec& x);
-RcppExport SEXP _meNet_rcpparma_innerproduct(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_innerproduct(x));
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_bothproducts
-Rcpp::List rcpparma_bothproducts(const arma::colvec& x);
-RcppExport SEXP _meNet_rcpparma_bothproducts(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_bothproducts(x));
-    return rcpp_result_gen;
-END_RCPP
-}
 // cor_meanSd
 NumericMatrix cor_meanSd(NumericMatrix data, int size, bool replace, int n_repetitions, double zero_precision);
 RcppExport SEXP _meNet_cor_meanSd(SEXP dataSEXP, SEXP sizeSEXP, SEXP replaceSEXP, SEXP n_repetitionsSEXP, SEXP zero_precisionSEXP) {
@@ -80,10 +37,6 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_meNet_rcpparma_hello_world", (DL_FUNC) &_meNet_rcpparma_hello_world, 0},
-    {"_meNet_rcpparma_outerproduct", (DL_FUNC) &_meNet_rcpparma_outerproduct, 1},
-    {"_meNet_rcpparma_innerproduct", (DL_FUNC) &_meNet_rcpparma_innerproduct, 1},
-    {"_meNet_rcpparma_bothproducts", (DL_FUNC) &_meNet_rcpparma_bothproducts, 1},
     {"_meNet_cor_meanSd", (DL_FUNC) &_meNet_cor_meanSd, 5},
     {"_meNet_cor_prTest", (DL_FUNC) &_meNet_cor_prTest, 4},
     {NULL, NULL, 0}
